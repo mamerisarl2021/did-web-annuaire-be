ff42ab0cd2485c7a9083989d719d651bac58edab
ctrldavieddee <princedavealex.20@gmail.com>
2026-03-18 16:14:46 +0100

    update jar package in jar

--- jwk_extract/build.gradle.kts
+++ jwk_extract/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.bouncycastle:bcprov-jdk18on:1.83")
     //implementation("org.json:json:20251224")
-    implementation("tools.jackson.core:jackson-databind:3.0.4")
+    implementation("tools.jackson.core:jackson-databind:3.1.0")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.83")
 }
 
